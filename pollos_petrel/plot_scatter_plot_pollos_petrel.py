6403936a3d8bef8e1bb6c242b4f8ae9df467863a
MemoOlv <guolg@ier.unam.mx>
2022-02-24 14:30:46 -0600

    Reformating file plot_scatter_plot_pollos_petrel.py using black

--- pollos_petrel/plot_scatter_plot_pollos_petrel.py
+++ pollos_petrel/plot_scatter_plot_pollos_petrel.py
@@ -3,5 +3,6 @@ import seaborn as sns
 
 training_dataset = pd.read_csv("train.csv")
 
-sns.scatterplot(data=training_dataset, x='Longitud_tarso',
-                y='Longitud_ala', hue='target')
+sns.scatterplot(
+    data=training_dataset, x="Longitud_tarso", y="Longitud_ala", hue="target"
+)
