b00b893f7a8fc188334ff47c19be917e8be7b2c5
MemoOlv <guolg@ier.unam.mx>
2022-02-24 13:47:09 -0600

    Creating plot_scatter_plot_pollos_petrel.py file

---
+++ pollos_petrel/plot_scatter_plot_pollos_petrel.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import seaborn as sns
+
+training_dataset = pd.read_csv("train.csv")
+
+sns.scatterplot(data=training_dataset, x='Longitud_tarso',
+                y='Longitud_ala', hue='target')
