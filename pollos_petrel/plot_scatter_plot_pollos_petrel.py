1c0b7660f65cb690a29de3415179ef0c2fb0ea2f
MemoOlv <guolg@ier.unam.mx>
2022-02-27 16:07:43 -0600

    Reformat plot_scatter_plot_pollos_petrel.py

--- pollos_petrel/plot_scatter_plot_pollos_petrel.py
+++ pollos_petrel/plot_scatter_plot_pollos_petrel.py
@@ -1,7 +1,6 @@
-# import pandas as pd
 import seaborn as sns
 from .dummy_model import read_training_dataset
 
 
-def scatter_plot(xlabel, ylabels, hue):
-    return sns.scatterplot(data=read_training_dataset(), x=xlabels, y=ylabels, hue=hue)
+def scatter_plot(xlabel, ylabel, hue):
+    return sns.scatterplot(data=read_training_dataset(), x=xlabel, y=ylabel, hue=hue)
