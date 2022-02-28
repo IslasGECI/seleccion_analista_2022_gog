11985a58d8be8f2b9af6cb284fa8bc7632e6a685
MemoOlv <guolg@ier.unam.mx>
2022-02-28 16:24:18 -0600

    Format plot_scatter_plot_pollos_petrel.py in Black 100 lines

--- pollos_petrel/plot_scatter_plot_pollos_petrel.py
+++ pollos_petrel/plot_scatter_plot_pollos_petrel.py
@@ -6,8 +6,4 @@ from .dummy_model import read_training_dataset
 def scatter_plot(xlabel, ylabel, hue):
     fig, ax = plt.subplots()
     sns.scatterplot(data=read_training_dataset(), x=xlabel, y=ylabel, hue=hue)
-<<<<<<< HEAD
     return fig, ax
-=======
-    return fig, ax
->>>>>>> 1680258127c3e9c18cc6d5df78381dcebce990c1
