afc67b20cf0eb0ec6e88f1794b40843175475e92
MemoOlv <guolg@ier.unam.mx>
2022-02-28 16:01:30 -0600

    Format test_plot_scatter_plot_pollos_petrel.py in Black 100 lines

---
+++ tests/test_plot_scatter_plot_pollos_petrel.py
@@ -0,0 +1,8 @@
+import pytest
+from pollos_petrel import read_training_dataset, scatter_plot
+
+
+def test_scatter_plot():
+    fig, ax = scatter_plot("Longitud_ala", "Longitud_tarso", "target")
+    assert ax.get_xlabel() == "Longitud_ala"
+    assert ax.get_ylabel() == "Longitud_tarso"
