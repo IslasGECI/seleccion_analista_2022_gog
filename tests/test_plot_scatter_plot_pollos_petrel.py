dce931a0dde1d0ccc3c3f7792d0a8ae650e9a19e
MemoOlv <53709062+MemoOlv@users.noreply.github.com>
2022-02-28 21:18:35 -0600

    Add expected and obtained variables in test_plot_tarsus_vs_wing function
    Co-authored-by: Evaristo Rojas • GECI <evaristo.rojas@islas.org.mx>

--- tests/test_plot_scatter_plot_pollos_petrel.py
+++ tests/test_plot_scatter_plot_pollos_petrel.py
@@ -13,5 +13,9 @@ def test_scatter_plot():
 
 def test_plot_tarsus_vs_wing():
     fig, ax = plot_tarsus_vs_wing()
-    assert ax.get_xlabel() == "Longitud_ala"
-    assert ax.get_ylabel() == "Longitud_tarso"
+    expected_xlabel = "Longitud_ala"
+    obtained_xlabel = ax.get_xlabel()
+    assert expected_xlabel == obtained_xlabel
+    expected_ylabel = "Longitud_tarso"
+    obtained_ylabel = ax.get_ylabel()
+    assert expected_ylabel == obtained_ylabel
