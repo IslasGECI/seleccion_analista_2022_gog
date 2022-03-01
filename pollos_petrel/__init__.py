1f4deb61561c5965161a51cf3994ea6d8165ea31
MemoOlv <53709062+MemoOlv@users.noreply.github.com>
2022-02-28 20:06:02 -0600

    Use * and noqa importing functions from do_nothing
    Co-authored-by: Evaristo Rojas • GECI <evaristo.rojas@islas.org.mx>

--- pollos_petrel/__init__.py
+++ pollos_petrel/__init__.py
@@ -1,6 +1,6 @@
 """A dummy Python module"""
 
 __version__ = "0.1.0"
-from .do_nothing import return_one  # noqa
+from .do_nothing import *  # noqa
 from .dummy_model import *  # noqa
 from .plot_scatter_plot_pollos_petrel import *  # noqa
