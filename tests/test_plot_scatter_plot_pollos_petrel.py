2a39b44d89ca7b720ecb99f07924d49d9deb3e4c
MemoOlv <53709062+MemoOlv@users.noreply.github.com>
2022-02-28 20:14:00 -0600

    Add function test_plot_tarsus_vs_wing
    Adding function test_plot_tarsus_vs_wing to test function plot_tarsus_vs_wing
    
    Co-authored-by: Evaristo Rojas • GECI <evaristo.rojas@islas.org.mx>

--- tests/test_plot_scatter_plot_pollos_petrel.py
+++ tests/test_plot_scatter_plot_pollos_petrel.py
@@ -5,3 +5,9 @@ def test_scatter_plot():
     fig, ax = scatter_plot("Longitud_ala", "Longitud_tarso", "target")
     assert ax.get_xlabel() == "Longitud_ala"
     assert ax.get_ylabel() == "Longitud_tarso"
+
+
+def test_plot_tarsus_vs_wing():
+    fig, ax = plot_tarsus_vs_wing()
+    assert ax.get_xlabel() == "Longitud_ala"
+    assert ax.get_ylabel() == "Longitud_tarso"
