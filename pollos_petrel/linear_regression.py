693af9a59afa4d72d83ebc1992d618e42646b062
MemoOlv <guolg@ier.unam.mx>
2022-03-04 12:38:22 -0600

    :sparkles: Add train_linear_model function

--- pollos_petrel/linear_regression.py
+++ pollos_petrel/linear_regression.py
@@ -1,3 +1,14 @@
+from scipy.optimize import curve_fit
+import pandas as pd
+
+
 # Modelo lineal
 def linear_model(x, slope, y_intercept):
     return slope * x + y_intercept
+
+
+# Entrenar modelo lineal
+def train_linear_model(dataset: pd.DataFrame):
+    clean_dataset = dataset[~dataset.Masa.isnull()]
+    parameters = curve_fit(linear_model, clean_dataset.Masa, clean_dataset.target)[0]
+    return parameters
