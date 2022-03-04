9437fefdfc46b9c85ee019a382ba8f7ca62fcb3b
MemoOlv <53709062+MemoOlv@users.noreply.github.com>
2022-03-04 11:23:10 -0600

    Split variables to readability
    Co-authored-by: Evaristo Rojas • GECI <evaristo.rojas@islas.org.mx>

--- tests/test_linear_regression.py
+++ tests/test_linear_regression.py
@@ -2,6 +2,9 @@ from pollos_petrel import linear_model
 
 
 def test_linear_model():
-    obtained_result = linear_model(2, 1, 3)
+    x = 2
+    slope = 1
+    y_intercept = 3
+    obtained_result = linear_model(x, slope, y_intercept)
     expected_result = 5
     assert obtained_result == expected_result
