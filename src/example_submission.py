b86cee3873d4b4d2685ed0caae23a8efd2ce1e17
MemoOlv <guolg@ier.unam.mx>
2022-03-11 15:44:43 -0600

    :adhesive_bandage: Fix importation error

--- src/example_submission.py
+++ src/example_submission.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-from pollos_petrel import add_mean_as_target, write_submission
-from .dummy_model import Path_To_Submission
+from pollos_petrel import add_mean_as_target, write_submission, Path_To_Submission
 
 write_submission(Path_To_Submission().DummyModel, add_mean_as_target)
