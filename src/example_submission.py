4b651ad647d2815ce8ff68f73138929d69db4a1c
MemoOlv <guolg@ier.unam.mx>
2022-03-11 15:18:31 -0600

    :recycle: Refactor write_submission function

--- src/example_submission.py
+++ src/example_submission.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
-from pollos_petrel import write_submission
+from pollos_petrel import add_mean_as_target, write_submission
+from .dummy_model import Path_To_Submission
 
-write_submission()
+write_submission(Path_To_Submission().DummyModel, add_mean_as_target)
