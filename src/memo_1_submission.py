b684bf7bb8761d0e487845377cb4016c0d2c1925
MemoOlv <guolg@ier.unam.mx>
2022-03-13 01:20:47 -0600

    :arrow_down: Remove memo_1_submission.py file added into make_submission.py

--- src/memo_1_submission.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-from pollos_petrel import predict_age_pollos_petrel, write_submission, Path_To_Submission
-
-write_submission(Path_To_Submission().LinearModel, predict_age_pollos_petrel)
